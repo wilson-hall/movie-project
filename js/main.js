b1597337a58207cd5f96b2ac1e12085d8bbc569a
Brandon <bran.tyler.hall@gmail.com>
2022-12-09 17:16:10 -0600

    added attribute toggling on buttons; PUT/PATCH in progress

--- js/main.js
+++ js/main.js
@@ -7,21 +7,54 @@
 //Display a "loading..." message
 // Make an AJAX request to get a listing of all the movies
 // When the initial AJAX request comes back, remove the "loading..." message and replace it with HTML generated from the json response your code receives
+
+const edit = "<a href='#'>Edit</a>"
+
 $(document).ready(() => {
     $('div#movies').html("Loading...")
 })
 fetch("https://lizard-positive-cook.glitch.me/movies").then(resp => resp.json()).then(data => {
+    // variable declaration
     const titles = data.map(data => data.title)
     const ratings = data.map(data => data.rating)
+
+    // empties the container of whatever is currently inside it
     $("#movies").empty();
+
+    // refills the container with every movie that has been passed to server
     for (let i = 0; i < data.length; i++) {
-        $("#movies").append(`${titles[i]}${ratings[i]}`)
+        $("#movies").append(`<p><span class="ref-title">${titles[i]}</span> <span class="ref-rating">${ratings[i]}</span> ${edit}</p>`)
     }
+
+    // edit button functionality
+    $('a').click(function (e) {
+        e.preventDefault();
+        let refTitle = $(this).siblings('span.ref-title').html();
+        let refRating = $(this).siblings('span.ref-rating').html();
+
+        console.log(refTitle);
+        console.log(refRating);
+
+        $('#title').val(refTitle);
+        $('#rating').val(refRating);
+
+        $('button').toggleClass('hidden')
+
+        $('#submit-button').attr('id', 'save-button');
+        $('#add-movie').attr('id', 'edit-movie');
+    })
+
     console.log(titles)
     console.log(ratings)
     console.log(data)
 })
 
+// save button functionality
+$('#save-button').click(() => {
+    $('button').toggleClass('hidden')
+    $('#save-button').attr('id', 'submit-button');
+    $('#edit-movie').attr('id', 'add-movie');
+})
 
 //CREATE - POST
 // Allow users to add new movies
@@ -43,18 +76,17 @@ $("#add-movie").submit(e => {
         body: JSON.stringify(newMovie),
     })
         .then(() => fetch("https://lizard-positive-cook.glitch.me/movies")
-        .then(resp => resp.json())
-        .then(data => {
-            const newTitles = data.map(data => data.title)
-            const newRatings = data.map(data => data.rating)
-            $("#movies").empty();
-            for (let i = 0; i < data.length; i++) {
-                $("#movies").append(`${newTitles[i]}${newRatings[i]}`)
-            }
-            console.log(newMovie)
-            console.log(data)
-    })
-        .catch(error => console.error(error)))
+            .then(resp => resp.json())
+            .then(data => {
+                const newTitles = data.map(data => data.title)
+                const newRatings = data.map(data => data.rating)
+                $("#movies").empty();
+                for (let i = 0; i < data.length; i++) {
+                    $("#movies").append(`<p><span class="ref-title">${newTitles[i]}</span> <span class="ref-rating">${newRatings[i]}</span> ${edit}</p> `)
+                }
+                console.log(newMovie)
+                console.log(data)
+            }))
 })
 
 
@@ -64,6 +96,33 @@ $("#add-movie").submit(e => {
 // Give users the option to edit an existing movie
 // A form should be pre-populated with the selected movie's details
 // Like creating a movie, this should not involve any page reloads, instead your javascript code should make an ajax request when the form is submitted.
+
+$("#edit-movie").submit(e => {
+    e.preventDefault()
+    const editedMovie = {
+        title: $("#title").val(),
+        rating: $("#rating").val()
+    };
+    fetch(`https://lizard-positive-cook.glitch.me/movies/1`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editedMovie),
+    })
+        .then(() => fetch("https://lizard-positive-cook.glitch.me/movies")
+            .then(resp => resp.json())
+            .then(data => {
+                const editedTitles = data.map(data => data.title)
+                const editedRatings = data.map(data => data.rating)
+                $("#movies").empty();
+                for (let i = 0; i < data.length; i++) {
+                    $("#movies").append(`<p><span class="ref-title">${editedTitles[i]}</span> <span class="ref-rating">${editedRatings[i]}</span>`);
+                }
+                console.log(editedMovie)
+                console.log(data)
+            }))
+})
 
 //DELETE
 // Delete movies
