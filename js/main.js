b3fec07ed7846907cbe78f62dcd521e58b71b238
Brandon <bran.tyler.hall@gmail.com>
2022-12-12 13:41:49 -0500

    refactored functionality

--- js/main.js
+++ js/main.js
@@ -10,105 +10,108 @@
 
 const edit = "<a href='#' class='edit'>Edit</a>"
 const deleteBtn = "<a href='#' class='delete'>Delete</a>"
+
 let idArr = [];
 
 $(document).ready(() => {
     $('div#movies').html("Loading...")
 })
-fetch("https://lizard-positive-cook.glitch.me/movies").then(resp => resp.json()).then(data => {
-    // variable declaration
-    const titles = data.map(data => data.title)
-    const ratings = data.map(data => data.rating)
-    const id = data.map(data => data.id)
-    console.log(id)
+const getMovies = () => {
+    fetch("https://lizard-positive-cook.glitch.me/movies").then(resp => resp.json()).then(data => {
+        // variable declaration
+        const titles = data.map(data => data.title)
+        const ratings = data.map(data => data.rating)
+        const id = data.map(data => data.id)
+        console.log(id)
+        // empties the container of whatever is currently inside it
+        $("#movies").empty();
 
-    // empties the container of whatever is currently inside it
-    $("#movies").empty();
-
-    // refills the container with every movie that has been passed to server
-    for (let i = 0; i < data.length; i++) {
-        $("#movies").append(`<p><span class="ref-title">${titles[i]}</span> <span class="ref-rating">${ratings[i]}</span> <span class="ref-id">${id[i]}</span> ${edit} ${deleteBtn}</p>`)
-    }
-
-    // edit button functionality
-    $('.edit').click(function (e) {
-        e.preventDefault();
-        let refTitle = $(this).siblings('span.ref-title').html();
-        let refRating = $(this).siblings('span.ref-rating').html();
-        let refID = $(this).siblings('span.ref-id').html();
-        idArr.push(refID)
-        console.log(refID)
-        console.log(refTitle);
-        console.log(refRating);
-
-        $('#title').val(refTitle);
-        $('#rating').val(refRating);
-
-        $('button').toggleClass('hidden')
-
-        $('#submit-button').attr('id', 'save-button');
-        $('#add-movie').attr('id', 'edit-movie');
-    })
-
-    $(".delete").click(function (e) {
-        e = confirm("Are you sure you want to delete this movie?")
-        let refID = $(this).siblings('span.ref-id').html();
-        idArr.push(refID)
-        console.log(refID)
-        console.log(idArr)
-        if (e) {
-            fetch(`https://lizard-positive-cook.glitch.me/movies/${idArr}`, {
-                method: "DELETE"
-            }).then(() => fetch("https://lizard-positive-cook.glitch.me/movies")).then(resp => resp.json()).then(movies => {
-                console.log(movies)
-                idArr = []
-            });
+        // refills the container with every movie that has been passed to server
+        for (let i = 0; i < data.length; i++) {
+            $("#movies").append(`<p><span class="ref-title">${titles[i]}</span> <span class="ref-rating">${ratings[i]}</span> <span class="ref-id">${id[i]}</span> ${edit} ${deleteBtn}</p>`)
         }
-    })
 
-    console.log(titles)
-    console.log(ratings)
-    console.log(data)
-}).then(() => {
-    //UPDATE - PUT/PATCH
+        // edit button functionality
+        $('.edit').click(function (e) {
+            e.preventDefault();
+            let refTitle = $(this).siblings('span.ref-title').html();
+            let refRating = $(this).siblings('span.ref-rating').html();
+            let refID = $(this).siblings('span.ref-id').html();
+            idArr.push(refID)
+            console.log(idArr);
+            console.log(refID)
+            console.log(refTitle);
+            console.log(refRating);
+
+            $('#title').val(refTitle);
+            $('#rating').val(refRating);
+
+            $('#submit-button').toggleClass('hidden');
+        });
+
+        $(".delete").click(function (e) {
+            e = confirm("Are you sure you want to delete this movie?")
+            let refID = $(this).siblings('span.ref-id').html();
+            idArr.push(refID)
+            console.log(refID)
+            console.log(idArr)
+            if (e) {
+                fetch(`https://lizard-positive-cook.glitch.me/movies/${idArr}`, {
+                    method: "DELETE"
+                }).then(() => {
+                    idArr = []
+                    getMovies();
+                });
+            } else {
+                idArr = []
+                console.log(idArr);
+                getMovies();
+            }
+        })
+
+        console.log(titles)
+        console.log(ratings)
+        console.log(data)
+
+        $("#save-button").click(e => {
+            e.preventDefault()
+            const editedMovie = {
+                title: $("#title").val(),
+                rating: $("#rating").val()
+            }
+            console.log(idArr)
+
+            fetch(`https://lizard-positive-cook.glitch.me/movies/${idArr}`, {
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(editedMovie)
+            }).then(() => {
+                // const editedTitles = data.map(data => data.title)
+                // const editedRatings = data.map(data => data.rating)
+                // const id = data.map(data => data.id)
+                idArr = []
+                // console.log(idArr)
+
+                $('#title').val('')
+                $('#rating').val('')
+                $('#save-button').toggleClass('hidden');
+
+                getMovies();
+            });
+        });
+    });
+}
+
+getMovies();
+
+//UPDATE - PUT/PATCH
 // Allow users to edit existing movies
 //
 // Give users the option to edit an existing movie
 // A form should be pre-populated with the selected movie's details
 // Like creating a movie, this should not involve any page reloads, instead your javascript code should make an ajax request when the form is submitted.
-$("#save-button").click(e => {
-    e.preventDefault()
-    const editedMovie = {
-        title: $("#title").val(),
-        rating: $("#rating").val()
-    }
-    console.log(idArr)
-
-    fetch(`https://lizard-positive-cook.glitch.me/movies/${idArr}`, {
-        method: "PUT",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(editedMovie)
-    }).then(() => fetch("https://lizard-positive-cook.glitch.me/movies")).then(resp => resp.json()).then(data => {
-        const editedTitles = data.map(data => data.title)
-        const editedRatings = data.map(data => data.rating)
-        idArr = []
-        console.log(idArr)
-        $("#movies").empty();
-        for (let i = 0; i < data.length; i++) {
-            $("#movies").append(`<p><span class="ref-title">${editedTitles[i]}</span> <span class="ref-rating">${editedRatings[i]}</span>${edit}</p>`);
-        }
-    });
-})
-})
-
-// save button functionality
-$('#save-button').click(() => {
-    $('button').toggleClass('hidden')
-    $('#save-button').attr('id', 'submit-button');
-    $('#edit-movie').attr('id', 'add-movie');
-})
 
 //CREATE - POST
 // Allow users to add new movies
