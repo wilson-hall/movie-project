786646adc74570579b018d2aa566a7c8a4a37ce3
Brandon <bran.tyler.hall@gmail.com>
2022-12-12 17:32:23 -0500

    begun styling webpage

--- js/main.js
+++ js/main.js
@@ -12,13 +12,12 @@ const getMovies = () => {
         const titles = data.map(data => data.title)
         const ratings = data.map(data => data.rating)
         const id = data.map(data => data.id)
-        console.log(id)
         // empties the container of whatever is currently inside it
         $("#movies").empty();
 
         // refills the container with every movie that has been passed to server
         for (let i = 0; i < data.length; i++) {
-            $("#movies").append(`<p><span class="ref-title">${titles[i]}</span> <span class="ref-rating">${ratings[i]}</span> <span class="ref-id">${id[i]}</span> ${edit} ${deleteBtn}</p>`)
+            $("#movies").append(`<p class="card m-2 col-3 text-center"><span class="ref-title">${titles[i]}</span> <span class="ref-rating">${ratings[i]}</span> <span class="ref-id">${id[i]}</span> ${edit} ${deleteBtn}</p>`)
         }
 
         // edit button functionality
@@ -27,11 +26,6 @@ const getMovies = () => {
             let refTitle = $(this).siblings('span.ref-title').html();
             let refRating = $(this).siblings('span.ref-rating').html();
             let refID = $(this).siblings('span.ref-id').html();
-
-
-            console.log(refID)
-            console.log(refTitle);
-            console.log(refRating);
             $("#movie-form").html(saveForm)
 
             $('#title').val(refTitle);
@@ -42,7 +36,6 @@ const getMovies = () => {
         $(".delete").click(function (e) {
             e = confirm("Are you sure you want to delete this movie?")
             let refID = $(this).siblings('span.ref-id').html();
-            console.log(refID)
             if (e) {
                 fetch(`https://lizard-positive-cook.glitch.me/movies/${refID}`, {
                     method: "DELETE"
@@ -98,7 +91,5 @@ $("#add-movie").submit(e => {
             .then(resp => resp.json())
             .then(data => {
                 getMovies();
-                console.log(newMovie)
-                console.log(data)
             }))
 });
