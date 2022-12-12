3cb83289469702e80c13864a20508c3dbd83e870
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-12-12 17:02:32 -0600

    working with omdb api

--- js/main.js
+++ js/main.js
@@ -12,8 +12,13 @@ const getMovies = () => {
         const titles = data.map(data => data.title)
         const ratings = data.map(data => data.rating)
         const id = data.map(data => data.id)
+        console.log(data)
+
         // empties the container of whatever is currently inside it
         $("#movies").empty();
+
+        // TODO
+        moviePosters(`${titles[i]}`)
 
         // refills the container with every movie that has been passed to server
         for (let i = 0; i < data.length; i++) {
@@ -46,8 +51,16 @@ const getMovies = () => {
                 getMovies();
             }
         })
+        addMovieFunctionality();
     });
 };
+
+const moviePosters = (refTitle) => {
+    fetch(`https://www.omdbapi.com/?t=${refTitle}&apikey=${omdbKey}`).then(resp => resp.json()).then((data) => {
+        console.log(data)
+        $(".card").prepend(`<img src=${data.poster}>`)
+    })
+}
 
 const saveButtonFunctionality = (refID) => {
     $("#edit-movie").submit(e => {
@@ -74,22 +87,24 @@ const saveButtonFunctionality = (refID) => {
 
 getMovies();
 
-$("#add-movie").submit(e => {
-    e.preventDefault()
-    const newMovie = {
-        title: $("#title").val(),
-        rating: $("#rating").val()
-    };
-    fetch("https://lizard-positive-cook.glitch.me/movies", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newMovie),
-    })
-        .then(() => fetch("https://lizard-positive-cook.glitch.me/movies")
-            .then(resp => resp.json())
-            .then(data => {
-                getMovies();
-            }))
-});
+const addMovieFunctionality = () => {
+    $("#add-movie").submit(e => {
+        e.preventDefault()
+        const newMovie = {
+            title: $("#title").val(),
+            rating: $("#rating").val()
+        };
+        fetch("https://lizard-positive-cook.glitch.me/movies", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+        })
+            .then(() => fetch("https://lizard-positive-cook.glitch.me/movies")
+                .then(resp => resp.json())
+                .then(data => {
+                    getMovies();
+                }))
+    });
+}
