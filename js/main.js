0ef09c88f876949c671495d52252cb9c7c91e432
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-12-13 12:31:30 -0600

    added image jpg. created js function that renders movie posters to the html from omdb api

--- js/main.js
+++ js/main.js
@@ -5,69 +5,85 @@ $(document).ready(() => {
 });
 
 const getMovies = () => {
-    fetch("https://lizard-positive-cook.glitch.me/movies").then(resp => resp.json()).then(data => {
-        $("#movie-form").html(submitForm)
+    fetch("https://lizard-positive-cook.glitch.me/movies")
+        .then(resp => resp.json())
+        .then(data => {
+            $("#movie-form").html(submitForm)
 
-        // variable declaration
-        const titles = data.map(data => data.title)
-        const ratings = data.map(data => data.rating)
-        const id = data.map(data => data.id)
-        console.log(data)
+            // variable declaration
+            const titles = data.map(data => data.title)
+            const ratings = data.map(data => data.rating)
+            const id = data.map(data => data.id)
+            const posters = data.map(data => data.poster)
+            console.log(data)
 
-        // empties the container of whatever is currently inside it
-        $("#movies").empty();
+            // empties the container of whatever is currently inside it
+            $("#movies").empty();
 
-        // TODO
-        moviePosters(`${titles[i]}`)
-
-        // refills the container with every movie that has been passed to server
-        for (let i = 0; i < data.length; i++) {
-            $("#movies").append(`<p class="card m-2 col-3 text-center"><span class="ref-title">${titles[i]}</span> <span class="ref-rating">${ratings[i]}</span> <span class="ref-id">${id[i]}</span> ${edit} ${deleteBtn}</p>`)
-        }
-
-        // edit button functionality
-        $('.edit').click(function (e) {
-            e.preventDefault();
-            let refTitle = $(this).siblings('span.ref-title').html();
-            let refRating = $(this).siblings('span.ref-rating').html();
-            let refID = $(this).siblings('span.ref-id').html();
-            $("#movie-form").html(saveForm)
-
-            $('#title').val(refTitle);
-            $('#rating').val(refRating);
-            saveButtonFunctionality(refID);
-        });
-
-        $(".delete").click(function (e) {
-            e = confirm("Are you sure you want to delete this movie?")
-            let refID = $(this).siblings('span.ref-id').html();
-            if (e) {
-                fetch(`https://lizard-positive-cook.glitch.me/movies/${refID}`, {
-                    method: "DELETE"
-                }).then(() => {
-                    getMovies();
-                });
-            } else {
-                getMovies();
+            // refills the container with every movie that has been passed to server
+            for (let i = 0; i < data.length; i++) {
+                $("#movies").append(`<p class="card col-4 p-0 text-center"><img src="${posters[i]}"><span class="ref-title">${titles[i]}</span> <span class="ref-rating">${ratings[i]}</span> <span class="ref-id">${id[i]}</span> ${edit} ${deleteBtn}</p>`)
             }
-        })
-        addMovieFunctionality();
-    });
+
+            // edit button functionality
+            $('.edit').click(function (e) {
+                e.preventDefault();
+                let refTitle = $(this).siblings('span.ref-title').html();
+                let refRating = $(this).siblings('span.ref-rating').html();
+                let refID = $(this).siblings('span.ref-id').html();
+                let stars = document.getElementsByClassName('star');
+                $("#movie-form").html(saveForm)
+                // console.log(refRating)
+                // console.log(stars[0].value)
+
+                $('#title').val(refTitle);
+                // $('#rating').val(refRating);
+                for (let i = 0; i < stars.length; i++) {
+                    if (stars[i].value === refRating) {
+                        console.log(stars[i].value)
+                        console.log(refRating)
+                        $('input[name="star"]').eq(i).prop('checked', true);
+                        break;
+                    }
+                }
+                document.querySelector('input[name="star"]').value;
+                saveButtonFunctionality(refID);
+            });
+
+            $(".delete").click(function (e) {
+                e = confirm("Are you sure you want to delete this movie?")
+                let refID = $(this).siblings('span.ref-id').html();
+                if (e) {
+                    fetch(`https://lizard-positive-cook.glitch.me/movies/${refID}`, {
+                        method: "DELETE"
+                    }).then(() => {
+                        getMovies();
+                    });
+                } else {
+                    getMovies();
+                }
+            })
+            addMovieFunctionality();
+        });
 };
 
-const moviePosters = (refTitle) => {
-    fetch(`https://www.omdbapi.com/?t=${refTitle}&apikey=${omdbKey}`).then(resp => resp.json()).then((data) => {
-        console.log(data)
-        $(".card").prepend(`<img src=${data.poster}>`)
-    })
+const moviePosters = (title) => {
+        return fetch(`https://www.omdbapi.com/?t=${title}&apikey=${OMDB_API_KEY}`)
+            .then(resp => resp.json())
+            .then((poster) => {
+                console.log(poster)
+                return poster.Poster
+        })
 }
 
 const saveButtonFunctionality = (refID) => {
     $("#edit-movie").submit(e => {
         e.preventDefault()
+        moviePosters($("#title").val()).then((poster) => {
         const editedMovie = {
             title: $("#title").val(),
-            rating: $("#rating").val()
+            rating: document.querySelector('input[name="star"]:checked').value,
+            poster
         }
 
         fetch(`https://lizard-positive-cook.glitch.me/movies/${refID}`, {
@@ -83,6 +99,7 @@ const saveButtonFunctionality = (refID) => {
             getMovies();
         });
     });
+    });
 };
 
 getMovies();
@@ -90,9 +107,11 @@ getMovies();
 const addMovieFunctionality = () => {
     $("#add-movie").submit(e => {
         e.preventDefault()
+        moviePosters($("#title").val()).then((poster) => {
         const newMovie = {
             title: $("#title").val(),
-            rating: $("#rating").val()
+            rating: document.querySelector('input[name="star"]:checked').value,
+            poster
         };
         fetch("https://lizard-positive-cook.glitch.me/movies", {
             method: 'POST',
@@ -106,5 +125,6 @@ const addMovieFunctionality = () => {
                 .then(data => {
                     getMovies();
                 }))
+    });
     });
 }
