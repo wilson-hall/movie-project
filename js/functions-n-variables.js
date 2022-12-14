03324f056e0a01fc8b8d48933312dab66bd3e544
Brandon <bran.tyler.hall@gmail.com>
2022-12-14 12:44:39 -0500

    added finishing touches

--- js/functions-n-variables.js
+++ js/functions-n-variables.js
@@ -34,30 +34,6 @@ const saveForm = () => {
 </form>`
 };
 
-// const submitForm = () => {
-//     return `<form id="add-movie">
-// \t<label for="title"> Enter the movie title
-// \t\t<input type="text" name="title" id="title">
-// \t\t</label>
-// \t<br>
-// \t<input type="radio" id="1-star" name="star" value="1"
-// \t\t>
-// \t<label for="1-star">1</label>
-// \t<input type="radio" id="2-star" name="star" value="2"
-// \t\t>
-// \t<label for="2-star">2</label>
-// \t<input type="radio" id="3-star" name="star" value="3"
-// \t\t>
-// \t<label for="3-star">3</label>
-// \t<input type="radio" id="4-star" name="star" value="4"
-// \t\t>
-// \t<label for="4-star">4</label>
-// \t<input type="radio" id="5-star" name="star" value="5"
-// \t\t>
-// \t<label for="5-star">5</label>
-// \t<button type='submit' id='submit-button' className='hidden'>Submit</button>
-// </form>`
-// };
 const submitForm = () => {
     return `<form id="add-movie" class="footer-form">
 \t<label for="title"> Add a movie
@@ -88,26 +64,3 @@ const submitForm = () => {
 const starImg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">\n' +
     '  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>\n' +
     '</svg>'
-
-
-// const saveForm = () => {
-//     return "<form id=\"edit-movie\">\n" +
-//         "\t<label for=\"title\"> Enter the movie title\n" +
-//         "\t\t<input type=\"text\" name=\"title\" id=\"title\" value=\"\">\n" +
-//         "\t</label>\n" +
-//         "\t<br>\n" +
-//         "\t<label for=\"rating\"> Enter your rating for this movie\n" +
-//         "\t\t<input type=\"text\" name=\"rating\" id=\"rating\" value=\"\">\n" +
-//         "\t</label><button type=\"submit\" id=\"save-button\" class=\"hidden\">Save</button></form>"
-// };
-
-// const submitForm = () => {
-//     return "<form id=\"add-movie\">\n" +
-//         "\t<label for=\"title\"> Enter the movie title\n" +
-//         "\t\t<input type=\"text\" name=\"title\" id=\"title\">\n" +
-//         "\t</label>\n" +
-//         "\t<br>\n" +
-//         "\t<label for=\"rating\"> Enter your rating for this movie\n" +
-//         "\t\t<input type=\"text\" name=\"rating\" id=\"rating\">\n" +
-//         "\t</label><button type='submit' id='submit-button' className='hidden'>Submit</button></form>"
-// };
--- js/main.js
+++ js/main.js
@@ -1,139 +1,141 @@
 "use strict";
+(() => {
 
-$(document).ready(() => {
-    $('div#movies').html("Loading...")
-});
+    $(document).ready(() => {
+        $('div#movies').html(`<div class="spinner-border position-absolute top-50 start-50 translate-middle" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>`)
+    });
 
-const getMovies = () => {
-    fetch("https://billowy-unmarred-coreopsis.glitch.me/movies")
-        .then(resp => resp.json())
-        .then(data => {
-            $("#movie-form").html(submitForm)
+    const getMovies = () => {
+        fetch("https://billowy-unmarred-coreopsis.glitch.me/movies")
+            .then(resp => resp.json())
+            .then(data => {
+                $("#movie-form").html(submitForm)
 
-            // variable declaration
-            const titles = data.map(data => data.title)
-            const ratings = data.map(data => data.rating)
-            const id = data.map(data => data.id)
-            const posters = data.map(data => data.poster)
-            console.log(data)
+                // variable declaration
+                const titles = data.map(data => data.title)
+                const ratings = data.map(data => data.rating)
+                const id = data.map(data => data.id)
+                const posters = data.map(data => data.poster)
 
-            // empties the container of whatever is currently inside it
-            $("#movies").empty();
+                // empties the container of whatever is currently inside it
+                $("#movies").empty();
 
-            // refills the container with every movie that has been passed to server
-            for (let i = 0; i < data.length; i++) {
-                $("#movies").append(`<div class="card col-4 col-lg-3 col-xl-2 p-0 text-center justify-content-between"><img src="${posters[i]}"><span class="ref-title">${titles[i]}</span> <span class="show-rating">${turnRatingIntoStars(ratings[i])}</span> <input type="hidden" class="ref-rating" value="${ratings[i]}"><span class="ref-id">${id[i]}</span> <div>${edit} ${deleteBtn}</div></div>`)
-            }
+                // refills the container with every movie that has been passed to server
+                for (let i = 0; i < data.length; i++) {
+                    $("#movies").append(`<div class="card col-4 col-lg-3 col-xl-2 p-0 text-center justify-content-between"><img src="${posters[i]}"><span class="ref-title">${titles[i]}</span> <span class="show-rating">${turnRatingIntoStars(ratings[i])}</span> <input type="hidden" class="ref-rating" value="${ratings[i]}"><span class="ref-id">${id[i]}</span> <div>${edit} ${deleteBtn}</div></div>`)
+                }
 
-            // edit button functionality
-            $('.edit').click(function (e) {
-                e.preventDefault();
-                let refTitle = $(this).parent().siblings('span.ref-title').html();
-                let refRating = $(this).parent().siblings('input.ref-rating').val();
-                let refID = $(this).parent().siblings('span.ref-id').html();
-                let stars = document.getElementsByClassName('star');
-                $("#movie-form").html(saveForm)
+                // edit button functionality
+                $('.edit').click(function (e) {
+                    e.preventDefault();
+                    let refTitle = $(this).parent().siblings('span.ref-title').html();
+                    let refRating = $(this).parent().siblings('input.ref-rating').val();
+                    let refID = $(this).parent().siblings('span.ref-id').html();
+                    let stars = document.getElementsByClassName('star');
+                    $("#movie-form").html(saveForm)
 
-                $('#title').val(refTitle);
-                for (let i = 0; i < stars.length; i++) {
-                    if (stars[i].value === refRating) {
-                        $('input[name="star"]').eq(i).prop('checked', true);
-                        break;
+                    $('#title').val(refTitle);
+                    for (let i = 0; i < stars.length; i++) {
+                        if (stars[i].value === refRating) {
+                            $('input[name="star"]').eq(i).prop('checked', true);
+                            break;
+                        }
                     }
-                }
-                document.querySelector('input[name="star"]').value;
-                saveButtonFunctionality(refID);
-            });
+                    document.querySelector('input[name="star"]').value;
+                    saveButtonFunctionality(refID);
+                });
 
-            $(".delete").click(function (e) {
-                e = confirm("Are you sure you want to delete this movie?")
-                let refID = $(this).parent().siblings('span.ref-id').html();
-                if (e) {
-                    $(this).parent().parent().slideUp();
-                    fetch(`https://billowy-unmarred-coreopsis.glitch.me/movies/${refID}`, {
-                        method: "DELETE"
-                    }).then(() => {
+                $(".delete").click(function (e) {
+                    e.preventDefault();
+                    e = confirm("Are you sure you want to delete this movie?")
+                    let refID = $(this).parent().siblings('span.ref-id').html();
+                    if (e) {
+                        $(this).parent().parent().slideUp();
+                        fetch(`https://billowy-unmarred-coreopsis.glitch.me/movies/${refID}`, {
+                            method: "DELETE"
+                        }).then(() => {
+                            getMovies();
+                        });
+                    } else {
                         getMovies();
-                    });
-                } else {
-                    getMovies();
-                }
-            })
-            addMovieFunctionality();
-            turnRatingIntoStars();
-        });
-};
-
-const moviePosters = (title) => {
-    return fetch(`https://www.omdbapi.com/?t=${title}&apikey=${OMDB_API_KEY}`)
-        .then(resp => resp.json())
-        .then((poster) => {
-            console.log(poster)
-            if (poster.Response === "False") {
-                poster.Poster = 'img/default.jpeg'
-            }
-            return poster.Poster
-        });
-}
-
-const saveButtonFunctionality = (refID) => {
-    $("#edit-movie").submit(e => {
-        e.preventDefault()
-        moviePosters($("#title").val()).then((poster) => {
-            const editedMovie = {
-                title: $("#title").val(),
-                rating: document.querySelector('input[name="star"]:checked').value,
-                poster
-            }
-
-            fetch(`https://billowy-unmarred-coreopsis.glitch.me/movies/${refID}`, {
-                method: "PUT",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(editedMovie)
-            }).then(() => {
-                $('#title').val('')
-                $('#rating').val('')
-
-                getMovies();
+                    }
+                })
+                addMovieFunctionality();
+                turnRatingIntoStars();
             });
-        });
-    });
-};
+    };
 
-getMovies();
-
-const addMovieFunctionality = () => {
-    $("#add-movie").submit(e => {
-        e.preventDefault()
-        moviePosters($("#title").val()).then((poster) => {
-            console.log(poster)
-            const newMovie = {
-                title: $("#title").val(),
-                rating: document.querySelector('input[name="star"]:checked').value,
-                poster
-            };
-            fetch("https://billowy-unmarred-coreopsis.glitch.me/movies", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(newMovie),
-            })
-                .then(() => fetch("https://billowy-unmarred-coreopsis.glitch.me/movies")
-                    .then(resp => resp.json())
-                    .then(data => {
-                        getMovies();
-                    }))
-        });
-    });
-}
-
-const turnRatingIntoStars = (num) => {
-    let htmlStr = ''
-    for (num; num > 0; num--) {
-        htmlStr += starImg
+    const moviePosters = (title) => {
+        return fetch(`https://www.omdbapi.com/?t=${title}&apikey=${OMDB_API_KEY}`)
+            .then(resp => resp.json())
+            .then((poster) => {
+                if (poster.Response === "False") {
+                    poster.Poster = 'img/default.jpeg'
+                }
+                return poster.Poster
+            });
     }
-    return htmlStr
-}
+
+    const saveButtonFunctionality = (refID) => {
+        $("#edit-movie").submit(e => {
+            e.preventDefault()
+            moviePosters($("#title").val()).then((poster) => {
+                const editedMovie = {
+                    title: $("#title").val(),
+                    rating: document.querySelector('input[name="star"]:checked').value,
+                    poster
+                }
+
+                fetch(`https://billowy-unmarred-coreopsis.glitch.me/movies/${refID}`, {
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(editedMovie)
+                }).then(() => {
+                    $('#title').val('')
+                    $('#rating').val('')
+
+                    getMovies();
+                });
+            });
+        });
+    };
+
+    getMovies();
+
+    const addMovieFunctionality = () => {
+        $("#add-movie").submit(e => {
+            e.preventDefault()
+            moviePosters($("#title").val()).then((poster) => {
+                const newMovie = {
+                    title: $("#title").val(),
+                    rating: document.querySelector('input[name="star"]:checked').value,
+                    poster
+                };
+                fetch("https://billowy-unmarred-coreopsis.glitch.me/movies", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(newMovie),
+                })
+                    .then(() => fetch("https://billowy-unmarred-coreopsis.glitch.me/movies")
+                        .then(resp => resp.json())
+                        .then(data => {
+                            getMovies();
+                        }))
+            });
+        });
+    }
+
+    const turnRatingIntoStars = (num) => {
+        let htmlStr = ''
+        for (num; num > 0; num--) {
+            htmlStr += starImg
+        }
+        return htmlStr
+    }
+})();
